a7c43370687a99f8fbcc08791d508509c5c92745
LaisGalvao <laisgbueno62@gmail.com>
2024-08-30 17:33:55 -0300

    aula 3 e 4 - variáveis e tipos de dados - funções, parâmetros e retorno

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-var nome string
+func sayTheName(s string) {
+	fmt.Println("Olá", s)
+}
+
+func convertStringToNumber(s string) (n int) {
+	i, _ := strconv.Atoi(s)
+	n = i
+	return
+}
 
 func main() {
-
-	nome = "Lais"
-	fmt.Println("Olá", nome)
+	n := convertStringToNumber("123")
+	fmt.Println("O número convertido é:", n)
+	sayTheName("Laís")
 }
